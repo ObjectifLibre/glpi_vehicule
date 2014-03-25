cf4dc38348d24de37c4cc70d554e2b928ecc4426
David Durieux <d.durieux@siprossii.com>
2014-03-25 15:10:43 +0100

    Plugin structure

---
+++ hook.php
@@ -0,0 +1,15 @@
+<?PHP
+
+function plugin_vehicule_install() {
+
+   return TRUE;
+}
+
+
+
+// Uninstall process for plugin : need to return TRUE if succeeded
+function plugin_vehicule_uninstall() {
+
+}
+
+?>
---
+++ setup.php
@@ -0,0 +1,47 @@
+<?php
+
+define ("PLUGIN_VEHICULE_VERSION", "0.85+1.0");
+
+include_once(GLPI_ROOT."/inc/includes.php");
+
+// Init the hooks of vehicule
+function plugin_init_vehicule() {
+   global $PLUGIN_HOOKS, $CFG_GLPI;
+
+
+}
+
+
+
+// Name and Version of the plugin
+function plugin_version_vehicule() {
+   return array('name'           => 'Example',
+                'shortname'      => 'exampl',
+                'version'        => PLUGIN_VEHICULE_VERSION,
+                'license'        => 'AGPLv3+',
+                'author'         =>'',
+                'homepage'       =>'http://',
+                'minGlpiVersion' => '0.85'
+   );
+}
+
+
+
+// Optional : check prerequisites before install : may print errors or add to message after redirect
+function plugin_vehicule_check_prerequisites() {
+   global $DB;
+
+   if (version_compare(GLPI_VERSION, '0.85', 'lt') || version_compare(GLPI_VERSION, '0.86', 'ge')) {
+      echo __('Your GLPI version not compatible, require 0.85', 'vehicule');
+      return FALSE;
+   }
+   return TRUE;
+}
+
+
+
+function plugin_vehicule_check_config() {
+   return TRUE;
+}
+
+?>
